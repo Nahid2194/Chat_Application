d0420aee0518ba98f908399b351f54a115ff6c75
Nahid2194 <nahidjc80@gmail.com>
2021-02-19 22:45:06 +0600

    complete ChatFeed jsx file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 const App = () => {
   return (
     <ChatEngine
@@ -8,6 +9,7 @@ const App = () => {
       projectID="121b74e9-cf22-468e-a932-2d4e2491056b"
       userName="Nahidjc"
       userSecret="Nahid2194"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 
     />
   )
